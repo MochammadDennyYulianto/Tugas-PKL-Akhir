3e127663b370b309a711b9de1003d42c42e5a89b
Mochammad Denny Yulianto <dennyyulianto23@gmail.com>
2022-06-21 13:35:35 +0700

    Merubah letak forgot password, merubah placeholder dan membuat method forgot password

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,6 +6,9 @@ class Auth extends CI_Controller
 	public function index()
 	{
 		$this->load->view('auth/auth-login');
+
+        // $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        // $this->form_validation->set_rules('password', 'Password', 'required');
 	}
 
     public function registration()
--- application/views/auth/auth-login.php
+++ application/views/auth/auth-login.php
@@ -122,27 +122,27 @@
                 </a>
               </div>
               <!-- /Logo -->
-              <h4 class="mb-2">Welcome to DenZal-! 👋</h4>
-              <p class="mb-4">Please sign-in to your account and start the adventure</p>
+              <h4 class="mb-5 text-center" >Welcome to this web</h4>
+              <p class="mb-3">Enter your email and password to login</p>
 
               <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
-                <div class="mb-3">
-                  <label for="email" class="form-label">Email or Username</label>
+                <div class="mb-4  ">
+                  <label for="email" class="form-label">Email</label>
                   <input
                     type="text"
                     class="form-control"
                     id="email"
-                    name="email-username"
-                    placeholder="Enter your email or username"
+                    name="email"
+                    placeholder="Enter your email in here"
                     autofocus
                   />
                 </div>
-                <div class="mb-3 form-password-toggle">
+                <div class="mb-4 form-password-toggle">
                   <div class="d-flex justify-content-between">
                     <label class="form-label" for="password">Password</label>
-                    <a href="auth-forgot-password-basic.html">
+                    <!-- <a href="auth-forgot-password-basic.html">
                       <small>Forgot Password?</small>
-                    </a>
+                    </a> -->
                   </div>
                   <div class="input-group input-group-merge">
                     <input
@@ -150,23 +150,28 @@
                       id="password"
                       class="form-control"
                       name="password"
-                      placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                      placeholder="Enter your password in here"
                       aria-describedby="password"
                     />
                     <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                   </div>
                 </div>
                 <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                  <button class="btn btn-primary d-grid w-100" type="submit">Log in</button>
                 </div>
               </form>
 
-              <p class="text-center">
-                <span>New on our platform?</span>
-                <a href="<?= base_url('auth/registration'); ?>">
-                  <span>Create an account</span>
-                </a>
-              </p>
+              <div class="d-flex justify-content-between">
+                  <p class="text-center">
+                    <a href="<?= base_url('auth/forgotpassword'); ?>">
+                      <span>Forgot password?</span>
+                    </a>
+                  </p>
+                  <p class="text-center">
+                    <a href="<?= base_url('auth/registration'); ?>">
+                      <span>Create an account</span>
+                    </a>
+                  </p>
             </div>
           </div>
           <!-- /Register -->
